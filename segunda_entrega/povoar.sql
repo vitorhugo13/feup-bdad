3969f324aee8592a2f904dbbc32ddb33a14b9f4b
unknown <vhlg99@gmail.com>
2019-04-11 17:30:14 +0100

    Added complement

--- segunda_entrega/povoar.sql
+++ segunda_entrega/povoar.sql
@@ -46,7 +46,10 @@ insert into Reservation (reservationID, creationDate, finalPrice, isPaid, client
 insert into Reservation (reservationID, creationDate, finalPrice, isPaid, client, complement) values (16,'2018-12-08', '€2928,73', 1, 19, 2);
 
 -- Complement
-
+insert into Complement (complementID, type, extraCost) values (4,'All included', '€150,00' );
+insert into Complement (complementID, type, extraCost) values (3,'Full board', '€105,00' );
+insert into Complement (complementID, type, extraCost) values (2,'Half board', '€55,00' );
+insert into Complement (complementID, type, extraCost) values (1,'Only accomodation', '€0,00' );
 -- Stay
 
 -- Guest
