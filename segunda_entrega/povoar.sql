0036905e14b0f0b5ad07d24220bc4a5e8c1882d6
unknown <vhlg99@gmail.com>
2019-04-11 17:17:43 +0100

    Fixed description of photos

--- segunda_entrega/povoar.sql
+++ segunda_entrega/povoar.sql
@@ -64,14 +64,14 @@ insert into Reservation (reservationID, creationDate, finalPrice, isPaid, client
 -- Feature
 
 -- Photo
-insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (1, '2018-05-11', 'Intuitive clear-thinking adapter', 'luctus.jpeg', NULL, 1, NULL);
-insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (2, '2018-12-26', 'Re-engineered demand-driven hierarchy', 'vel.gif', NULL, NULL, 1);
-insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (3, '2018-06-13', 'Face to face multimedia website', 'rhoncus_aliquet_pulvinar.tiff', NULL, 2, NULL);
-insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (4, '2018-08-09', 'Upgradable eco-centric hierarchy', 'rutrum_at.jpeg', NULL, NULL, 2);
-insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (5, '2018-11-09', 'Versatile 4th generation core', 'lacinia_eget.png', 5, NULL, NULL);
-insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (6, '2018-12-03', 'Open-source fresh-thinking artificial intelligence', 'ut_nunc_vestibulum.tiff', NULL, NULL, 6);
-insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (7, '2018-07-02', 'Function-based static initiative', 'sit_amet_sapien.tiff', NULL, 7, NULL);
-insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (8, '2018-11-19', 'Cross-platform tertiary contingency', 'augue_vestibulum_ante.gif', 8, NULL, NULL);
-insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (9, '2018-09-01', 'Robust radical encoding', 'libero_rutrum_ac.gif', 9, NULL, NULL);
-insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (10, '2018-11-25', 'Face to face discrete migration', 'ipsum_dolor_sit.tiff', NULL, NULL, 10);
+insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (1, '2018-05-11', 'Presidential suite view', 'luctus.jpeg', NULL, 1, NULL);
+insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (2, '2018-12-26', 'Presidential suite bed', 'vel.gif', NULL, NULL, 1);
+insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (3, '2018-06-13', 'President suite bathroom', 'rhoncus_aliquet_pulvinar.tiff', NULL, 2, NULL);
+insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (4, '2018-08-09', 'Executive room', 'rutrum_at.jpeg', NULL, NULL, 2);
+insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (5, '2018-11-09', 'Restaurant', 'lacinia_eget.png', 5, NULL, NULL);
+insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (6, '2018-12-03', 'Swimming pool', 'ut_nunc_vestibulum.tiff', NULL, NULL, 6);
+insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (7, '2018-07-02', 'Reception', 'sit_amet_sapien.tiff', NULL, 7, NULL);
+insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (8, '2018-11-19', 'Singleroom view', 'augue_vestibulum_ante.gif', 8, NULL, NULL);
+insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (9, '2018-09-01', 'Doubleroom bathroom', 'libero_rutrum_ac.gif', 9, NULL, NULL);
+insert into Photo (photoID, date, description, photoPath, meetingRoom, bedroom, client) values (10, '2018-11-25', 'Hotel entrance', 'ipsum_dolor_sit.tiff', NULL, NULL, 10);
 
