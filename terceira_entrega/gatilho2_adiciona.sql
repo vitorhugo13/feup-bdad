de31663fdeb409c0de6088407837359a07fa98c4
Vitor Hugo <vhlg99@gmail.com>
2019-05-24 22:47:42 +0100

    just for the lols

--- terceira_entrega/gatilho2_adiciona.sql
+++ terceira_entrega/gatilho2_adiciona.sql
@@ -1,9 +1,4 @@
 --data_de_cancelamento;
---se a data de cancelamento se verificar, isto é, for anterior à data de inicio da estadia, o seu isAllowed é colocado a 1 e o stay correspondente é apagado.
---para o trigger funcionar é preciso:
--- ver que a data de inicio da estadia é inferior à de cancelamento
---se sim, colocar isALlowed a 1
---por fim eliminar do Stay essa reserva
 
 CREATE TRIGGER CancelDate
 AFTER INSERT ON CancelLing
