fbd1fbfc65a6cc2c237e1168405546611e9a639a
Miguel Simões <miguelsimoes20000720@gmail.com>
2019-05-25 00:13:25 +0100

    Half of the trigger 2 is good

--- terceira_entrega/gatilho2_adiciona.sql
+++ terceira_entrega/gatilho2_adiciona.sql
@@ -3,7 +3,9 @@
 CREATE TRIGGER CancelDate
 BEFORE INSERT ON Cancelling
 FOR EACH ROW
-WHEN (julianDay(Cancelling.cancelDate) < (SELECT julianDay(Stay.startDate) WHERE Cancelling.reservation = Stay.reservation))
+WHEN ((New.cancelDate) < (SELECT startDate FROM Stay WHERE reservation=New.reservation))
 BEGIN
+
         DELETE FROM Stay WHERE New.reservation = Stay.reservation;      
 END;
+
