8bfeabeac4a3b352c4f8de013bd29c62ed82f048
Miguel Simões <miguelsimoes20000720@gmail.com>
2019-05-25 01:40:12 +0100

    Trigger 2 is good

--- terceira_entrega/gatilho2_adiciona.sql
+++ terceira_entrega/gatilho2_adiciona.sql
@@ -1,11 +1,18 @@
 --data_de_cancelamento;
 
 CREATE TRIGGER CancelDate
-BEFORE INSERT ON Cancelling
+AFTER INSERT ON Cancelling
 FOR EACH ROW
 WHEN ((New.cancelDate) < (SELECT startDate FROM Stay WHERE reservation=New.reservation))
 BEGIN
-
-        DELETE FROM Stay WHERE New.reservation = Stay.reservation;      
+        DELETE FROM Stay WHERE New.reservation = Stay.reservation;
 END;
 
+
+CREATE TRIGGER CancelDateError
+AFTER INSERT ON Cancelling
+FOR EACH ROW
+WHEN ((New.cancelDate) >= (SELECT startDate FROM Stay WHERE reservation=New.reservation))
+BEGIN
+        DELETE FROM Cancelling WHERE New.reservation = Cancelling.reservation;
+END;
--- terceira_entrega/gatilho2_verifica.sql
+++ terceira_entrega/gatilho2_verifica.sql
@@ -6,6 +6,7 @@ SELECT * FROM Cancelling;
 SELECT * FROM Stay;
 
 INSERT INTO Cancelling (reservation, client, cancelDate, cost) values (15, 20 , '2019-11-20', '13,99');
+INSERT INTO Cancelling (reservation, client, cancelDate, cost) values (16, 20 , '2019-12-20', '13,99');
 
 SELECT * FROM Cancelling;
 SELECT * FROM Stay;
