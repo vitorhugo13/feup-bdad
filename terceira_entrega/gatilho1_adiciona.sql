dcdc6757c0f06e2d03dea851f34c3f86866d462b
Miguel Simões <miguelsimoes20000720@gmail.com>
2019-05-25 05:01:54 +0100

    Date expression

--- terceira_entrega/gatilho1_adiciona.sql
+++ terceira_entrega/gatilho1_adiciona.sql
@@ -6,52 +6,31 @@ FOR EACH ROW
 BEGIN
     UPDATE Reservation
     SET finalPrice = (
-        (SELECT price FROM (SELECT room FROM RoomStay
-        WHERE stay = (SELECT reservation from Stay Where reservation = Old.reservation)))
-        --(SELECT Room FROM RoomStay As RS INNER JOIN Stay As S WHERE RS.stay = S.Reservation))
-        + (SELECT extraCost FROM Complement
-        WHERE complementID = (SELECT complement FROM Reservation WHERE reservationID = Old.reservationID))
-        )
-        * SELECT DATEDIFF(day, startDate FROM Stay Where reservation = Old.reservation, endDate FROM Stay Where reservation = Old.reservation)
-    WHERE Reservation.reservationID = Old.reservationID
+        select ((select  sum(price) 
+        from Reservation natural join Stay natural join RoomStay natural join Room 
+        where Reservation.reservationID = Stay.reservation 
+        and Stay.reservation = RoomStay.stay 
+        and RoomStay.room = Room.roomNumber 
+        and Reservation.reservationID = 1
+        ) + (
+        select  sum(extraCost)
+        from Reservation natural join Complement natural join Guest 
+        where Reservation.reservationID = 1 
+        and Reservation.complement = Complement.complementID 
+        and Reservation.reservationID = Guest.stay))
+    WHERE Reservation.reservationID = New.reservationID
 END;
 
-/*  PREÇO BASE
 
-select reservationID, sum(price) 
-from Reservation natural join Stay natural join RoomStay natural join Room 
-where Reservation.reservationID = Stay.reservation 
-and Stay.reservation = RoomStay.stay 
-and RoomStay.room = Room.roomNumber 
-and Reservation.reservationID = 1;
+/*  DATA
 
-    EXTRA
-
-select reservationID, sum(extraCost), guestID 
-from Reservation natural join Complement natural join Guest 
-where Reservation.reservationID = 1 
-and Reservation.complement = Complement.complementID 
-and Reservation.reservationID = Guest.stay;
-
-*/
-
-/*
-
-soma o valor do preço base com o valor extra!!
-
-
-select ((select  sum(price) 
-from Reservation natural join Stay natural join RoomStay natural join Room 
-where Reservation.reservationID = Stay.reservation 
-and Stay.reservation = RoomStay.stay 
-and RoomStay.room = Room.roomNumber 
-and Reservation.reservationID = 1
-)
-+
-(
-select  sum(extraCost)
-from Reservation natural join Complement natural join Guest 
-where Reservation.reservationID = 1 
-and Reservation.complement = Complement.complementID 
-and Reservation.reservationID = Guest.stay));
+select((select julianday(endDate) 
+from Stay natural join Reservation 
+where Reservation.reservationId = Stay.reservation 
+and Reservation.reservationId = 1) 
+-
+(select julianday(startDate) 
+from Stay natural join Reservation 
+where Reservation.reservationId = Stay.reservation 
+and Reservation.reservationId = 1));
 */
