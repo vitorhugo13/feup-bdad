9b11a343b7d4495f66e125a909003b6b55072a2a
Vitor Hugo <vhlg99@gmail.com>
2019-05-26 17:56:53 +0100

    stuff on trigger3

--- terceira_entrega/gatilho3_adiciona.sql
+++ terceira_entrega/gatilho3_adiciona.sql
@@ -1,7 +1,11 @@
 --verificar_num_guests;
 
 CREATE TRIGGER VerifyGuests
-ON 
-AFTER 
+AFTER INSERT ON --tabela
 FOR EACH ROW
+BEGIN
 
+END;
+
+--select * from RoomStay natual join Room natural join Guest 
+--where Room.roomNumber = room
