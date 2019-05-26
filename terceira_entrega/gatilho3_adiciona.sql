d2ffdc484e280b6224cc10a4798de4bc58306cce
Miguel Simões <miguelsimoes20000720@gmail.com>
2019-05-26 22:14:11 +0100

    Trigger 3 is done

--- terceira_entrega/gatilho3_adiciona.sql
+++ terceira_entrega/gatilho3_adiciona.sql
@@ -1,11 +1,31 @@
 --verificar_num_guests;
 
 CREATE TRIGGER VerifyGuests
-AFTER INSERT ON --tabela
-FOR EACH ROW
+AFTER INSERT ON Guest
+WHEN ((select count(stay) 
+    from (select stay, guestID, firstName, lastName 
+    from RoomStay natual join Room natural join Guest 
+    where Room.roomNumber = room group by stay, firstName, lastName)
+    where stay = New.stay)          -- Count the people in a reservation
+    > (
+    select sum(capacity) 
+    from RoomStay natural join Room 
+    where Room = roomNumber and stay = New.stay))           -- Count the people the reservation can have
 BEGIN
-
+    DELETE FROM Guest WHERE guestID = New.guestID and stay = New.stay;
 END;
 
---select * from RoomStay natual join Room natural join Guest 
---where Room.roomNumber = room
+/*
+SELECIONA Nº DE GUESTS QUE EXISTEM
+select * from RoomStay natual join Room natural join Guest 
+where Room.roomNumber = room
+*/
+
+
+/*
+--nº de guests associados a uma reserva
+select stay, count(stay) from (select stay, guestID, firstName, 
+lastName from RoomStay natual join Room natural join Guest 
+where Room.roomNumber = room group by stay,firstName, lastName)
+where stay = 1;
+*/
--- terceira_entrega/gatilho3_verifica.sql
+++ terceira_entrega/gatilho3_verifica.sql
@@ -1,3 +1,13 @@
 .mode       columns
 .headers    on
 .nullvalue  NULL
+
+SELECT * FROM Guest;
+
+insert into Guest values (1, 11, 'Chase', 'Kose');
+
+select * from Guest;
+
+insert into Guest values (2, 11, 'Vitor', 'Leite');
+
+SELECT * FROM Guest;
